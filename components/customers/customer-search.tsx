bb46609624630596cc73daf6864492d666bce374
Aiden Zepp <aiden.zep@gauntletai.com>
2025-01-26 15:27:19 -0500

    fix: remove unused useCallback import in customer search component

--- components/customers/customer-search.tsx
+++ components/customers/customer-search.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useTransition, useEffect, useState, useCallback } from "react"
+import { useTransition, useEffect, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { Input } from "@/components/ui/input"
 import { useDebounce } from "use-debounce"
